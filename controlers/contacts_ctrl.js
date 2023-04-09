c8dbf581b314b226cf7edc510e8f0723d2806dd9
Prysmytskyi-Oleksandr <san2003@ukr.net>
2023-04-09 12:43:04 +0300

    done

---
+++ controlers/contacts_ctrl.js
@@ -0,0 +1,63 @@
+const { Contact } = require("../models/contact");
+
+const { ctrlWrapper } = require("../decorator/index");
+const { HttpError } = require("../helpers");
+
+const getAllContacts = async (req, res) => {
+  const result = await Contact.find();
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { id } = req.params;
+  // const result = await Contact.findOne({ _id: id });
+  const result = await Contact.findById(id);
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+const deleteById = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await Contact.findByIdAndDelete(id);
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+  res.json({
+    message: "contact deleted",
+  });
+};
+
+const updateById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+  res.json(result);
+};
+
+const upDateFavoriteById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, `Contact with ${id} not found`);
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  deleteById: ctrlWrapper(deleteById),
+  updateById: ctrlWrapper(updateById),
+  updateFavoriteById: ctrlWrapper(upDateFavoriteById),
+};
