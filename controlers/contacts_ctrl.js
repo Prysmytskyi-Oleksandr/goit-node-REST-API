7c78ee56457bbb02efc6c84cc1a0c9b25cf23084
Prysmytskyi-Oleksandr <san2003@ukr.net>
2023-04-16 22:07:25 +0300

    done

--- controlers/contacts_ctrl.js
+++ controlers/contacts_ctrl.js
@@ -7,10 +7,10 @@ const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, { skip, limit }).populate(
-    "owner",
-    "name email"
-  );
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
   res.json(result);
 };
 
