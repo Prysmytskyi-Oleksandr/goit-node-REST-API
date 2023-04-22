493725cef316b9db49e66ad7e40a13e0f5ba0a23
Prysmytskyi-Oleksandr <san2003@ukr.net>
2023-04-22 16:28:48 +0300

    done

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,9 @@
 const authenticate = require("./authenticate");
 const authorization = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
   authenticate,
+  authorization,
+  upload,
 };
--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { validateBody } = require("../../decorator");
-const { authenticate } = require("../../middlewares");
+const { authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 const ctrl = require("../../controlers/auth_ctrl");
 
@@ -10,5 +10,6 @@ router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
 
 module.exports = router;
